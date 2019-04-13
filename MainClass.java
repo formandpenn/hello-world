ba45ae98f82978a7e48945c7951e8fd890c8b928
MamathaSrinath <mamatha@seas.upenn.edu>
2019-04-13 12:21:25 +0530

    Added the git fire strategy

--- MainClass.java
+++ MainClass.java
@@ -11,6 +11,12 @@ public class MainClass {
 		}
 		System.out.println("...now let's add some more chutzpah to this!");
 		
+		System.out.println("What if there was fire in the building?");
+		System.out.println("1. Git commit");
+		System.out.println("2. Git push");
+		System.out.println("3. Leave the building");
+		System.out.println("Simple, ain't it?");
+		
 		
 	}
 
