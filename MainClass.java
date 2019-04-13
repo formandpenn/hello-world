2ee3959b69b41999ca8397983950dbc18d4a9ed2
MamathaSrinath <mamatha@seas.upenn.edu>
2019-04-13 12:12:49 +0530

    Created a simple print statement

---
+++ MainClass.java
@@ -0,0 +1,17 @@
+
+public class MainClass {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i=1; i<6; i++) {
+			System.out.print(i + " ");
+			
+		}
+		System.out.println("...now let's add some more chutzpah to this!");
+		
+		
+	}
+
+}
